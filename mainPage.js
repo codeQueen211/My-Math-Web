16bf868936ca85169959371aca7e9984339c6d24
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-10-24 14:35:43 +0300

    fixing styling and adding pages for the main page buttons

--- mainPage.js
+++ mainPage.js
@@ -3,37 +3,22 @@ function openGames(){
     window.open(webpage, "_blank");
 }
 function openVideos(){
-    const webpage="GamesPage.html";
+    const webpage="videoPage.html";
     window.open(webpage, "_blank");
 }
 function openPDF(){
-    // // Create an HTML document
-    // const htmlDocument = document.implementation.createHTMLDocument('Dynamic Page');
-    // //Add html elements 
-    // const html = htmlDocument.documentElement;
-    // const head = htmlDocument.head;
-    // const title = htmlDocument.createElement('title');
-    // title.textContent = 'My Dynamic Page';
-    // const body = htmlDocument.body;
-    // const h1 = htmlDocument.createElement('h1');
-    // h1.textContent = "This is a dynamic page!";
-    // // Add the elements to the document
-    // head.appendChild(title);
-    // body.appendChild(h1);
-    // // Append the document to the current page
-    // document.body.appendChild(html);
-    const webpage="GamesPage.html";
+    const webpage="pdfPage.html";
     window.open(webpage, "_blank");
 }
 function openTests(){
-    const webpage="GamesPage.html";
+    const webpage="testPage.html";
     window.open(webpage, "_blank");
 }
 function openTracker(){
-    const webpage="GamesPage.html";
+    const webpage="trackerPage.html";
     window.open(webpage, "_blank");
 }
 function openHelp(){
-    const webpage="GamesPage.html";
+    const webpage="helpPage.html";
     window.open(webpage, "_blank");
 }
