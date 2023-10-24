9926c611bd349b29ada7ac88c1bb2678446d68f6
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-10-24 12:58:52 +0300

    Initial commit

---
+++ myweb.js
@@ -0,0 +1,24 @@
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+
+    };
+
+    const serviceID = "service_r0kn7t4";
+    const templateID = "template_6psxhwp";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+
+            console.log(res);
+            alert("Your message sent successfully!!")
+
+        })
+        .catch(err => console.log(err));
+
+}
+
